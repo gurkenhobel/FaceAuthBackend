f1aab8ba692afdffb65ee39bee4d943c2305a151
kautschuckflasche <nilsw.agner@web.de>
2017-03-29 14:49:02 +0200

    fixed datacontract

--- Contract/FileCreateRequestMessage.cs
+++ Contract/FileCreateRequestMessage.cs
@@ -3,6 +3,7 @@ namespace FaceAuthService.Contract
     class FileCreateRequestMessage : Message
     {
         public string name { get; set; }
+        public bool encrypted { get; set; }
         public string data {get;set;}
         public FileCreateRequestMessage()
         {
